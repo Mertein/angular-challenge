394003ec4bf8296a4ccbb12bcc6679459a94ce98
Martín Eduardo Aguayo <martineduardoaguayo@gmail.com>
2024-05-14 21:54:01 -0300

    09: Add MockUserLogin to loggin without backend to deploy

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,19 +11,39 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class AuthService {
+
+  public mockUserValid = {
+    email: 'user@demo.com',
+    password: '123456',
+  }
+
   constructor(private http: HttpClient) {}
 
+
   login(email: string, password: string): Observable<any> {
-    return this.http.get<any>('http://localhost:3000/signupUsersList').pipe(
-      map((res: []) => {
-        const user = res.find((a: any) => a.email === email && a.password === password);
-        if (user) {
-          return user;
-        } else {
-          throw new Error('Usuario no encontrado');
-        }
-      })
-    );
+
+    //This function is a mock function that simulates a login request to the server
+    if(email !== this.mockUserValid.email|| password !== this.mockUserValid.password) {
+      return new Observable((observer) => {
+        throw new Error('Usuario no encontrado');
+      });
+    } else {
+      return new Observable((observer) => {
+        observer.next(this.mockUserValid);
+        observer.complete();
+      });
+    }
+
+    // return this.http.get<any>('http://localhost:3000/signupUsersList').pipe(
+    //   map((res: []) => {
+    //     const user = res.find((a: any) => a.email === email && a.password === password);
+    //     if (user) {
+    //       return user;
+    //     } else {
+    //       throw new Error('Usuario no encontrado');
+    //     }
+    //   })
+    // );
   }
 
   // register(username: string, email: string, password: string): Observable<any> {
